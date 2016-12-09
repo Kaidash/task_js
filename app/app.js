51e4f84367dcff1ab7327f2405e6d5c55d7d3626
nikita <kaidash.nikita93@gmail.com>
2016-12-09 17:05:31 +0200

    for item

--- app/app.js
+++ app/app.js
@@ -8,29 +8,41 @@ function CollectionPosts() {
     [].forEach.call(x, function (el) {
         arr.push(eval(el.value))
     });
-
+    //array all objects
     var flattened=arr.reduce(function (a,b) {
         return a.concat(b)
     });
-
+    console.log(flattened);
     [].forEach.call(flattened, function (el) {
         var a_values;
         var b_values;
 
         for(var i=1;i<flattened.length;i++){
-             a_values = Object.keys(el).map(function(key){
-                return el[key];
-            });
-            b_values = Object.keys(flattened[i]).map(function(key){
-                return flattened[i][key];
-            });
-        }
+              Object.keys(el).map(function(key) {
+                  //this each all elements with this
+                  console.log('-------------');
+                  console.log(el[key]);
+                  console.log('-------------');
+                  [].forEach.call(flattened, function (el) {
 
+                          Object.keys(el).map(function(key) {
+                              //this each all elements with this
+                              console.log(el[key]);
+                          })
+
+
+                  });
+              })
+
+
+        }
+        // console.log(a_values);
+        // console.log(b_values);
         for (var a_key in a_values) {
            
 
             if (a_values[a_key]==b_values[a_key]) {
-                console.log(a_values[a_key])
+                // console.log(a_values[a_key])
             }
         }
 
